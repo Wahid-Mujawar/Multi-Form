0f75c8355bbe68b3f4ca79650659db89f078f79c
Wahid-Mujawar <abdulwahid0218@gmail.com>
2020-12-31 13:17:08 -0800

    test

--- src/Components/UserForm.js
+++ src/Components/UserForm.js
@@ -1,7 +1,49 @@
 import React, { Component } from 'react'
 
 export class UserForm extends Component {
+   state = {
+      step: 1,
+      firtsname: '',
+      lastname: '',
+      email: '',
+      occupation: '',
+      bio: '',
+      city: ''
+   }
+
+   //Proceed to next Step
+
+   nextStep = () => {
+      const {step} = this.state;
+      this.setState({
+         step: step + 1
+      });
+   }
+
+   //Go to pervious Step
+
+   previousStep = () => {
+      const {step} = this.state;
+      this.setState({
+         step: step - 1
+      });
+   }
+
+   //Handle fields change
+
+   handleChange = input => e => {
+      this.setState([input]: e.target.value);
+   }
+
+
    render() {
+      const { step } = this.state;
+      const { firtsname, lastname, email, occupation,bio,
+      city } = this.state;
+      const values = { firtsname, lastname, email, occupation,bio,
+      city } = this.state;
+
+
       return (
          <div>
             
