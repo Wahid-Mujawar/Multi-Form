5c4ea8a51f753ca65932b819a9472bcb2b2fa178
Wahid-Mujawar <abdulwahid0218@gmail.com>
2021-01-13 22:36:41 -0800

    test

--- src/Components/Confirm.js
+++ src/Components/Confirm.js
@@ -64,7 +64,7 @@ export class Confirm extends Component {
             />
 
             <RaisedButton
-               // label="Back"
+               label="Back"
                // primary={false}
                // style={styles.button}
                // onClick={this.back}
